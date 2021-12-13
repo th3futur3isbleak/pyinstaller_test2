437673929cb90bf7edd3e98bb580f155fd1a7121
Bleak Future <th3futur3isbleak@thefutureisbleak.net>
2021-12-12 19:44:29 -0500

    Fixed release suffix script.

--- scripts/print_release_suffix.sh
+++ scripts/print_release_suffix.sh
@@ -4,7 +4,7 @@ then
       # $HEAD_TAG is empty, use current version and abbrev commit for release suffix
       CURRENT_VERSION=$(python -c "import pyinstaller_test2;print(pyinstaller_test2.__version__)")
       ABBREV_COMMIT=$( git log -1 --pretty=format:%h)
-      echo "$CURRENT_VERSION-$ABBREV_COMMIT"
+      echo "v$CURRENT_VERSION-$ABBREV_COMMIT"
 else
       # HEAD is tagged, use that for release suffix
       echo "$HEAD_TAG"
