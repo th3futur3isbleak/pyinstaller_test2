e9f70424274461a1f734ddde2326be4245605dc8
Bleak Future <th3futur3isbleak@thefutureisbleak.net>
2021-12-13 20:56:48 -0500

    More release workflow testing.

--- scripts/print_release_suffix.sh
+++ scripts/print_release_suffix.sh
@@ -1,9 +1,9 @@
-ABBREV_COMMIT=$( git log -1 --pretty=format:%h)
-HEAD_TAG=$(git tag --points-at $ABBREV_COMMIT)
+HEAD_TAG=$(git tag --points-at HEAD)
 if [ -z "$HEAD_TAG" ]
 then
       # $HEAD_TAG is empty, use current version and abbrev commit for release suffix
       CURRENT_VERSION=$(python -c "import pyinstaller_test2;print(pyinstaller_test2.__version__)")
+      ABBREV_COMMIT=$( git log -1 --pretty=format:%h)
       echo "v$CURRENT_VERSION-$ABBREV_COMMIT"
 else
       # HEAD is tagged, use that for release suffix
