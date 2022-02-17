4f7b7f89c13f5bcb8c899224c6bddb3114599262
Bleak Future <th3futur3isbleak@thefutureisbleak.net>
2022-02-16 22:08:35 -0500

    major change, doubtful outcome

---
+++ scripts/bash/build_exe.sh
@@ -0,0 +1,34 @@
+if [ -z "$1" ]
+then
+    TEMP_DIR=".build_exe"
+else
+    TEMP_DIR=$1
+fi
+echo "Temp directory is  $TEMP_DIR"
+
+# Argument one is the path to the source file
+# Argument two is the name of the final executable
+# --onefile builds a bundle
+# --add-data includes files in the bundle
+# these files will not be in the temporary directory and
+# not visible to the user
+# --distpath changes the distribution directory so we have a
+# directory read to be copied somewhere with all the associated files
+# --workpath changes the build directory to avoid leaving artificacts in the main repo
+
+# create bundles directory if it doesn't exist
+mkdir -p $TEMP_DIR/dist/bundles
+
+# Finds ntc_templates directory
+NTC_TEMPLATES_DIR=$(python scripts/windows/get_ntc_templates_dir.py)
+echo "$NTC_TEMPLATES_DIR"
+
+pyinstaller pyinstaller_test2/show_version.py \
+	--onefile \
+	-n show_version \
+	--distpath  $TEMP_DIR/dist/bundles/show_version \
+	--workpath  $TEMP_DIR/build \
+	--specpath  $TEMP_DIR/dist \
+	--add-data $NTC_TEMPLATES_DIR:ntc_templates
+
+
